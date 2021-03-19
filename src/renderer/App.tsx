db7c278f4e92867cf71777e2174e9b9dceb97b7a
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-03-19 11:37:23 -0300

    App.tsx cleanup

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-// import { GlobalStyle } from './styles/GlobalStyle'
 
 import Greetings from "./components/Greetings"
-
-
-
-console.log("I WORKED");
 
 const App = () => {
   return (
